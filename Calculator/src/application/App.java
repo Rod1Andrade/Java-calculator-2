d9fcb569eb91e289bfee44b7d9e24c11b63bc045
Rodrigo Andrade <51142291+Rod1Andrade@users.noreply.github.com>
2020-11-28 11:03:31 -0300

    UPDATE - Classe de aplicação
    Classe principal da aplicação, utilizada para entry point.

---
+++ Calculator/src/application/App.java
@@ -0,0 +1,75 @@
+package application;
+
+import java.awt.Dimension;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Classe principal da aplicação.
+ * 
+ * @author Rodrigo M.P Andrade
+ *
+ */
+public class App extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	private Dimension screenDimension = this.getToolkit().getScreenSize();
+	
+	private static final double WIDTH_SCALE = .3;
+	private static final double HEIGHT_SCALE = .6;
+
+	/**
+	 * Construtor padrão da classe App
+	 */
+	private App(String title) {
+		this.setTitle(title);
+		this.setSize(this.getScaledScreenDimension());
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/**
+	 * Inicia os recursos definidos na classe.
+	 * 
+	 * @param component Componente inicial da aplicação.
+	 */
+	private void start(JComponent component) {
+		this.getContentPane().add(component);
+		this.setVisible(true);
+	}
+
+	/**
+	 * Retorna a largura escalonada de acordo com as preferencias
+	 * 
+	 * @return double
+	 */
+	private double getScaledWidth() {
+		return screenDimension.getWidth() * WIDTH_SCALE;
+	}
+
+	/**
+	 * Retorna a altura escalonada de acordo com as preferencias
+	 * 
+	 * @return double
+	 */
+	private double getScaledHeight() {
+		return screenDimension.getHeight() * HEIGHT_SCALE;
+	}
+
+	/**
+	 * Devolve a dimensao scalonada de acordo com as preferencias.
+	 * 
+	 * @return Dimension
+	 */
+	private Dimension getScaledScreenDimension() {
+		return new Dimension((int) this.getScaledWidth(), (int) this.getScaledHeight());
+	}
+
+	public static void main(String[] args) {
+		new App("Calculadora 2").start(new JPanel());
+	}
+
+}
