c16cb35e5435f276147d56305ec62f602e97b58a
Rodrigo Andrade <51142291+Rod1Andrade@users.noreply.github.com>
2020-12-01 09:07:09 -0300

    UPDATE - Valores constants

---
+++ Calculator/src/presenter/utils/PresenterConstants.java
@@ -0,0 +1,16 @@
+package presenter.utils;
+
+/**
+ * Valores constantes da camada de apresentação.
+ * 
+ * @author Rodrigo M.P Andrade
+ * @since 1 de dez. de 2020
+ *
+ */
+public class PresenterConstants {
+	public static final String DELETAR = "Deletar";
+	public static final String RESULT_VALUE = "Resultado";
+	public static final String DEFAULT_VALUE = "Nenhuma operação...";
+	public static final String SELECT_OPERATION = "Selecionar Operação";
+	public static final String DIVISION_BY_ZERO = "Não é possível dividir por zero.";
+}
